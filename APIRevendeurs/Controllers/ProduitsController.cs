d768b137ef369164549539f0458f887b39b7c0e6
Nathan Vabre <nathan.vabre@epsi.fr>
2023-02-23 18:47:04 +0100

    create project

---
+++ APIRevendeurs/Controllers/ProduitsController.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Runtime;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace APIRevendeurs.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProduitsController : ControllerBase
+    {
+        // GET: produits
+        [HttpGet]
+        public async Task<List<Produits>> GetAsync()
+        {
+            HttpClient client = new HttpClient();
+            var json = await client.GetStringAsync("https://63f39eecde3a0b242b461bef.mockapi.io/api/v1/products");
+            var data = JsonConvert.DeserializeObject<List<Produits>>(json);
+            return data;
+        }
+
+        // GET produits/id
+        [HttpGet("{id}")]
+        public async Task<Produits> Get(int id)
+        {
+            HttpClient client = new HttpClient();
+            var json = await client.GetStringAsync("https://63f39eecde3a0b242b461bef.mockapi.io/api/v1/products/" + id);
+            var data = JsonConvert.DeserializeObject<Produits>(json);
+            return data;
+        }
+    }
+}
